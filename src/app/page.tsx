697688cde130ca845361b7892284a101cf7cf786
Josué Brenes <josuebrenes3005@gmail.com>
2025-01-23 20:18:11 -0600

    feat: update main content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,11 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Login } from "@/components/auth/login";
-import { Register } from "@/components/auth/register";
-import { ForgotPassword } from "@/components/auth/forgot-password";
 import { DashboardHeader } from "@/components/layouts/dashboard-header";
 import { DashboardFooter } from "@/components/layouts/dashboard-footer";
 import { useGlobalAuthenticationStore } from "@/components/auth/store/data";
 
 export default function HomePage() {
-  const [authView, setAuthView] = useState<
-    "login" | "register" | "forgotPassword"
-  >("login");
   const [theme, setTheme] = useState<"light" | "dark">("light");
   const [language, setLanguage] = useState<"es" | "en" | "fr" | "de">("en");
   const router = useRouter();
@@ -31,11 +25,6 @@ export default function HomePage() {
     }
   }, [address, router]);
 
-  const handleLogin = () => {
-    console.log("User authenticated");
-    router.push("/dashboard");
-  };
-
   return (
     <div className={`h-screen flex flex-col ${theme === "dark" ? "dark" : ""}`}>
       <DashboardHeader
@@ -43,21 +32,24 @@ export default function HomePage() {
         setTheme={setTheme}
         setLanguage={setLanguage}
       />
-      <div className="flex-1 flex items-center justify-center bg-white dark:bg-[#18181B] text-black dark:text-gray-100">
-        {authView === "login" && (
-          <Login
-            onSwitchToRegister={() => setAuthView("register")}
-            onForgotPassword={() => setAuthView("forgotPassword")}
-            onLogin={handleLogin}
-          />
-        )}
-        {authView === "register" && (
-          <Register onSwitchToLogin={() => setAuthView("login")} />
-        )}
-        {authView === "forgotPassword" && (
-          <ForgotPassword onBackToLogin={() => setAuthView("login")} />
-        )}
-      </div>
+      <main className="flex-1 flex flex-col sm:flex-row justify-center items-center bg-white dark:bg-[#18181B] text-black dark:text-gray-100 mt-20 gap-10">
+        <h1 className="text-5xl md:text-6xl font-bold text-center md:text-left">
+          Welcome to <br /> TrustBridge
+        </h1>
+        <hr className="hidden md:block bg-gray-200 dark:bg-gray-600 w-0.5 h-96" />
+        <p className="text-xl md:w-1/2 text-gray-700 dark:text-gray-300 leading-relaxed text-center md:text-left">
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-blue-500 font-bold">
+            TrustBridge is a decentralized platform
+          </span>{" "}
+          designed to facilitate <strong>P2P microloans</strong> securely,
+          transparently, and efficiently. We connect lenders and borrowers
+          through <strong>blockchain technology</strong>, leveraging{" "}
+          <strong>smart contracts</strong> to automate and secure transactions.
+          Our approach promotes <strong>financial inclusion</strong>, empowering
+          global communities by providing accessible and reliable tools to
+          manage loans without traditional intermediaries.
+        </p>
+      </main>
       <DashboardFooter />
     </div>
   );
