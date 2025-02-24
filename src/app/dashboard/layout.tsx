e7c42f724379c9a29b178e2af7befc1a13a3f933
MPSxDev <mparedes.digital@gmail.com>
2025-02-24 14:16:45 -0300

    feat: Add RouteGuard to the dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,36 +1,33 @@
 "use client";
 
-import { useGlobalAuthenticationStore } from "@/components/auth/store/data";
 import { DashboardFooter } from "@/components/layouts/dashboard-footer";
 import { DashboardHeader } from "@/components/layouts/dashboard-header";
 import { DashboardSidebar } from "@/components/layouts/dashboard-sidebar";
-import { redirect } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { RouteGuard } from "@/components/auth/RouteGuard";
+import React, { useState } from "react";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  const { address } = useGlobalAuthenticationStore();
-
-  if (address === "") {
-    redirect("/");
-  }
-
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const [theme, setTheme] = useState<"light" | "dark">("dark");
   const [, setLanguage] = useState<"es" | "en" | "fr" | "de">("en");
 
   return (
-    <div className={`flex min-h-screen ${theme === "dark" ? "dark" : ""}`}>
-      <DashboardSidebar />
-      <div className="flex-1 flex flex-col">
-        <DashboardHeader
-          theme={theme}
-          setTheme={setTheme}
-          setLanguage={setLanguage}
-        />
-        {children}
-        <DashboardFooter />
+    <RouteGuard allowedRoles={['Borrower']}>
+      <div className={`flex min-h-screen ${theme === "dark" ? "dark" : ""}`}>
+        <DashboardSidebar />
+        <div className="flex-1 flex flex-col">
+          <DashboardHeader
+            theme={theme}
+            setTheme={setTheme}
+            setLanguage={setLanguage}
+          />
+          {children}
+          <DashboardFooter />
+        </div>
       </div>
-    </div>
+    </RouteGuard>
   );
-};
-
-export default Layout;
+}
