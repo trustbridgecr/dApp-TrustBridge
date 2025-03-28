9b5fb8674974cf1aa685c032519717b8f212dea1
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-03-28 10:47:46 -0600

    feat: add ESLint and Prettier configuration with Husky support

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,21 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    ignores: ["node_modules", "dist", "build", ".next"],
+  },
+  {
+    rules: {
+      "react/react-in-jsx-scope": "off",
+    },
+  },
 ];
-
-export default eslintConfig;
