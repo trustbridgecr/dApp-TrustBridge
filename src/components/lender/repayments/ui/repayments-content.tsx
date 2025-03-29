6d4a3af5171f9060e0fb0a4ac1990719483b1c9b
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-03-29 12:27:12 -0600

    fix: error in code

--- src/components/lender/repayments/ui/repayments-content.tsx
+++ src/components/lender/repayments/ui/repayments-content.tsx
@@ -1,34 +1,53 @@
-"use client";
+'use client';
 
-import { useState } from "react";
-import { SearchBar } from "./utils/search-bar";
-import { StatusFilter } from "./filters/status-filter";
-import { RepaymentsTable } from "./tables/repayments-table";
-import { Status } from "@/@types/repayment.entity";
+import { useState } from 'react';
+import { SearchBar } from './utils/search-bar';
+import { StatusFilter } from './filters/status-filter';
+import { RepaymentsTable } from './tables/repayments-table';
+import { Status } from '@/@types/repayment.entity';
+import { useTranslation } from 'react-i18next';
 
 export default function RepaymentsContent() {
-  const [status, setStatus] = useState<Status>("all");
-  const [searchQuery, setSearchQuery] = useState("");
+  const { t } = useTranslation();
+  const [status, setStatus] = useState<Status>('all');
+  const [searchQuery, setSearchQuery] = useState('');
 
   return (
-    <div className="p-6 space-y-6 dark:bg-darkbg">
-      <h1 className="text-3xl font-bold tracking-tight dark:text-gray-50">
-        Repayments
-      </h1>
+    <div className='p-6 space-y-8 sm:space-y-12 bg-gradient-to-b from-[#0B1120] via-[#0B1120] to-[#121E31] w-full'>
+      <div className='flex max-sm:flex-col justify-between sm:items-center max-sm:gap-8'>
+        <div className='relative inline-block w-fit'>
+          <h1 className='text-2xl sm:text-3xl font-bold bg-gradient-to-r from-[#63CDE6] to-[#6AD09E] text-transparent bg-clip-text'>
+            {t('repayments.title')}
+          </h1>
+          <div className='absolute -bottom-2 left-0 w-full h-[3px]'>
+            <div className='h-full w-full bg-white bg-gradient-to-r from-blue-600 to-green-400 rounded-full'></div>
+            <div className='h-full w-[60%] bg-white bg-gradient-to-r from-blue-600 to-green-400 mt-1 opacity-50 rounded-full'></div>
+          </div>
+        </div>
 
-      <div className="flex flex-col sm:flex-row gap-4 items-start sm:items-center justify-between">
-        <SearchBar onSearch={setSearchQuery} />
-        <StatusFilter selectedStatus={status} onStatusChange={setStatus} />
+        <button
+          type='button'
+          className='text-white border border-[#63CDE6]/20 bg-[#0F1927] max-sm:self-end flex space-x-2 items-center shadow-[0px_0px_12px_1px_rgba(99,205,230,0.25)] font-medium rounded-full text-sm px-3 sm:px-5 py-2.5 text-center me-2 mb-2'
+        >
+          <div className='h-3 w-3 sm:h-4 sm:w-4 bg-[#63CDE6] rounded-full'></div>
+          <span>{t('repayments.paymentTracking')}</span>
+        </button>
       </div>
 
-      <div className="rounded-lg dark:bg-darkbg border dark:bg-darkbg dark:border-gray-700">
-        <div className="p-6">
-          <h2 className="text-xl font-semibold tracking-tight dark:text-gray-100">
-            Repayments Overview
-          </h2>
-          <p className="text-sm text-muted-foreground mt-2">
-            Track and manage all loan repayments.
+      <div className='space-y-6 sm:space-y-10 w-full'>
+        <div className='rounded-2xl p-6 flex gap-4 flex-col md:flex-row justify-between md:items-center bg-[#0F1927] border  border-[#63CDE6]/20 shadow-[0px_0px_8px_2px_rgba(99,205,230,0.15)]'>
+          <p className='text-sm lg:text-base text-gray-300 mt-2'>
+            {t('repayments.subtitle')}
           </p>
+          <div className='flex flex-row max-sm:flex-col gap-4 items-start items-centers md:justify-between'>
+            <SearchBar onSearch={setSearchQuery} />
+            <div className='max-sm:self-end'>
+              <StatusFilter
+                selectedStatus={status}
+                onStatusChange={setStatus}
+              />
+            </div>
+          </div>
         </div>
         <RepaymentsTable searchQuery={searchQuery} statusFilter={status} />
       </div>
