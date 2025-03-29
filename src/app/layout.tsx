d0bbc98b79ea481c597449771173f33684d2171a
Thosine-01 <thosine7900>
2025-03-29 16:13:42 +0100

    Feature: Implement Borrower Dashboard with SHADCN and Tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import ClientLayout from "./layouts/client-layout";
@@ -6,11 +8,14 @@ import "./globals.css";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
+  display: "swap",
 });
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
+  display: "swap",
+
 });
 
 export const metadata: Metadata = {
