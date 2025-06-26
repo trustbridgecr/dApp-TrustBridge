f8576c8f3abdb357a54c3d2eb3ed4b82f3827bb3
Josué Brenes <josuebrenes3005@gmail.com>
2025-06-25 18:52:57 -0600

    feat: add custom hook useIsMobile to detect mobile device

---
+++ src/hooks/use-mobile.ts
@@ -0,0 +1,18 @@
+import { useEffect, useState } from "react";
+export function useIsMobile() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 768px)");
+    const handleChange = () => setIsMobile(mediaQuery.matches);
+
+    handleChange();
+    mediaQuery.addEventListener("change", handleChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleChange);
+    };
+  }, []);
+
+  return isMobile;
+}
