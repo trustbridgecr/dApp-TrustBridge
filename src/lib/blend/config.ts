29a0b9fe02d423d30aedabd8a9dc9f0e31e98385
Josué Brenes <123853174+JosueBrenes@users.noreply.github.com>
2025-06-25 18:28:45 -0600

    Remove Blend features and unused types

--- src/lib/blend/config.ts
+++
@@ -1,3 +0,0 @@
-export const FACTORY_ADDRESS = process.env.NEXT_PUBLIC_FACTORY_ADDRESS || "";
-export const ORACLE_ADDRESS = process.env.NEXT_PUBLIC_ORACLE_ADDRESS || "";
-export const DEFAULT_WALLET = process.env.NEXT_PUBLIC_DEFAULT_WALLET || "";
